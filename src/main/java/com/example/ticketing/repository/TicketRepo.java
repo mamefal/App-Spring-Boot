478a84163d3686d6ff1b03d8d952336ee3759609
Mamefal <matouf94@gmail.com>
2025-11-20 16:30:29 +0100

    feat: ajout repositories JPA (User, Ticket, Category, Comment)

---
+++ src/main/java/com/example/ticketing/repository/TicketRepo.java
@@ -0,0 +1,4 @@
+package com.example.ticketing;
+
+public interface TicketRepo {
+}
---
+++ src/main/java/com/example/ticketing/repository/UserRepo.java
@@ -0,0 +1,10 @@
+package com.example.ticketing.repo;
+
+import com.example.ticketing.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
